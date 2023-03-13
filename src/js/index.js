77e04e475342cf60cf6526eff89b6f29e66cd797
Matheus <13matheusilva@gmail.com>
2023-03-13 13:58:45 -0300

    js pronto

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+const buttonShare = document.querySelector(".fa-share")
+const share = document.querySelector(".share")
+
+buttonShare.addEventListener("click", function(){
+    share.classList.toggle("none")
+})
